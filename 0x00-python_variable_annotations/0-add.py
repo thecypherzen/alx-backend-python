1a3ade36cab78ea8a00177890f22a67847952bea
William Inyam <sw.inyam@outlook.com>
2024-10-11 04:16:33 +0000

    syle(0x00:0): enforce pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """A simple function"""
 
-def add(a:float, b:float) -> float:
+
+def add(a: float, b: float) -> float:
     """A function that adds two floats
     Returns sum as float
     """
