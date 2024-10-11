87211aa1353168909ae3aefe981c36e25035ac00
William Inyam <sw.inyam@outlook.com>
2024-10-11 05:09:30 +0000

    docs(0x00:9): add function docstring

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,4 +7,5 @@ from typing import List, Iterable, Tuple, Sequence
 
 def element_length(lst: Iterable[Sequence]) -> \
         List[Tuple[Sequence, int]]:
+    """Returns a list and a sequence"""
     return [(i, len(i)) for i in lst]
