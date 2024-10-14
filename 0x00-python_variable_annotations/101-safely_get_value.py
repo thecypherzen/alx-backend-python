a569e54e93b3039898fbb8becff997ee8069a1fa
William Inyam <sw.inyam@outlook.com>
2024-10-14 14:24:51 +0000

    docs(0x00:11): add function docstring

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -12,6 +12,9 @@ def safely_get_value(dct: Mapping,
                      key: Any,
                      default: Union[Custom, None] = None) -> \
                      Union[Any, Custom]:
+    """ A function that returns value at a key of a mapping
+        if it exists or None otherwise
+    """
     if key in dct:
         return dct[key]
     else:
