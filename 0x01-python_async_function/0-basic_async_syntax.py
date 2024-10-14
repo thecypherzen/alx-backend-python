ffc879d485c33a1e399713eafb65bceff054fa5c
William Inyam <sw.inyam@outlook.com>
2024-10-14 16:26:45 +0000

    docs(0x01:0): add annotations

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,8 +5,8 @@ from asyncio import create_task, sleep
 from random import uniform
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """Returns a number between 0 and max_delay"""
-    delay = uniform(0, max_delay)
+    delay: float = uniform(0, max_delay)
     await sleep(delay)
     return delay
