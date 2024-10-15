a1fb39c31fb3245ca7f27f971bb8f400c680715c
William Inyam <sw.inyam@outlook.com>
2024-10-15 03:42:31 +0000

    refactor(0x01:0): change use of random.uniform

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """The basics of async"""
 
-from asyncio import create_task, sleep
-from random import uniform
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """Returns a number between 0 and max_delay"""
-    delay: float = uniform(0, max_delay)
-    await sleep(delay)
+    delay: float = random.random() * max_delay
+    await asyncio.sleep(delay)
     return delay
