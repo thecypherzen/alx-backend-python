3e55512e0e40c59fae5c8b9b88a2594a627fd64f
William Inyam <sw.inyam@outlook.com>
2024-10-15 04:12:21 +0000

    style(0x01:1): extend annnotations

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """Executing multiple coroutines at the same time with async"""
-from typing import List
-wait_random = __import__("0-basic_async_syntax").wait_random
+from typing import Callable, Coroutine, List, Union
+wait_random: Callable[[int], Coroutine[None, None, float]] = \
+    __import__("0-basic_async_syntax").wait_random
 
 
-def insert_at(lst: list, val: float) -> int:
+def insert_at(lst: List[Union[float]], val: float) -> int:
     """returns the index in lst to insert val"""
     length: int = len(lst)
+    i: int = length - 1
     if not length:
         return 0
-    i = length - 1
     while i >= 0:
         if lst[i] <= val:
             break
@@ -28,7 +29,7 @@ async def wait_n(n: int, max_delay: int) -> List:
       list of all return values of <max_delay> sorted in ascending
       order without using sort
     """
-    result: List = []
+    result: List[Union[float]] = []
 
     for _ in range(n):
         delay: float = await wait_random(max_delay)
