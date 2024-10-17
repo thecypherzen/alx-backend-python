05ef65c472c74985a08f6178a23cf2415aa54ef7
William Inyam <sw.inyam@outlook.com>
2024-10-17 08:17:32 +0000

    build(0x01:3): add task solution

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Defines a function that returns an asyncio Task"""
+
+from asyncio import create_task, Task
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay) -> Task:
+    """Returns an anyncio Task"""
+    return create_task(wait_random(max_delay))
