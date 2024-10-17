5d1fb9911f3fa833b793902e4663dea832630741
William Inyam <sw.inyam@outlook.com>
2024-10-17 07:04:48 +0000

    build(0x01:2): add task solution and test

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,36 +1,22 @@
 #!/usr/bin/env python3
 """Executing multiple coroutines at the same time with async"""
-from typing import List
-wait_random = __import__("0-basic_async_syntax").wait_random
+
+from asyncio import run
+from time import time
+wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-def insert_at(lst: list, val: float) -> int:
-    """returns the index in lst to insert val"""
-    length: int = len(lst)
-    if not length:
-        return 0
-    i = length - 1
-    while i >= 0:
-        if lst[i] <= val:
-            break
-        i -= 1
-    return i + 1
-
-
-async def wait_n(n: int, max_delay: int) -> List:
-    """Creates as many coroutines as defined by <max_delay>
+def measure_time(n: int, max_delay: int) -> float:
+    """Measures the total execution time for
+    wait_n(n, max_delay).
 
     Parameters:
-      n(int): the number of times to call wait_random
-      max_delay(int): the delay value to pass to wait_random
+       n(int) - number of iterations
+       max_delay(int) - sleep time between each iteration
 
-    Returns:
-      list of all return values of <max_delay> sorted in ascending
-      order without using sort
+    Returns total_time / n
     """
-    result: List = []
-
-    for _ in range(n):
-        delay: float = await wait_random(max_delay)
-        result.insert(insert_at(result, delay), delay)
-    return result
+    before: float = time()
+    res: float = run(wait_n(n, max_delay))
+    after: float = time()
+    return (after - before)/n
