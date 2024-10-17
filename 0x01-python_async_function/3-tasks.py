e308b0c9f30f1b96ed11ad23272a2a2f363c4657
William Inyam <sw.inyam@outlook.com>
2024-10-17 08:18:53 +0000

    style(0x01:3): extend annotation

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,6 +5,6 @@ from asyncio import create_task, Task
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay) -> Task:
+def task_wait_random(max_delay: int) -> Task:
     """Returns an anyncio Task"""
     return create_task(wait_random(max_delay))
